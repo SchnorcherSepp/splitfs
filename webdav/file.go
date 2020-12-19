897ff217546b26328993b028aac5a377fba63e19
Schnorcher Sepp <SchnorcherSepp@post4me.at>
2020-12-19 21:20:47 +0100

    undo

--- webdav/file.go
+++ webdav/file.go
@@ -17,7 +17,6 @@ import (
 )
 
 var _ webdav.File = (*_File)(nil)
-var lagBuffer = make([]byte, 50*1024*1024) // TODO: WTF!
 
 // _File is returned by a FileSystem's OpenFile method and can be served by a Handler.
 type _File struct {
@@ -73,11 +72,6 @@ func (f *_File) Read(p []byte) (n int, err error) {
 		}
 		f.innerReader = rAt
 	}
-
-	// lag fix?
-	go func() {
-		_, _ = f.innerReader.ReadAt(lagBuffer, f.innerOff) // TODO: WTF!
-	}()
 
 	// return
 	n, err = f.innerReader.ReadAt(p, f.innerOff)
